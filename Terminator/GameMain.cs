30d780c5528b47b48f8ff3c0ab9bebfce1d7329f
linfuqing <linfuqing@126.com>
2025-01-02 16:50:09 +0800

    Update GameMain.cs

--- Terminator/GameMain.cs
+++ Terminator/GameMain.cs
@@ -76,10 +76,10 @@ public class GameMain : MonoBehaviour
         
         ContentDeliveryGlobalState.RegisterForContentUpdateCompletion(__OnContentUpdateCompletion);
 #else
-        //var assetManager = GameAssetManager.instance;
-        assetManager.onConfirmCancel += __OnConfirmCancel;
+        var gameAssetManager = GameAssetManager.instance;
+        gameAssetManager.onConfirmCancel += __OnConfirmCancel;
 
-        assetManager.StartCoroutine(__Init());
+        gameAssetManager.StartCoroutine(__Init());
 #endif
     }
 
