3d9cc07b33d5dfb3f5743e26ecae4b0083c33e3f
linfuqing <linfuqing@126.com>
2025-01-14 11:35:44 +0800

    version 0.17.38

--- Terminator/Managers/LoginManager.cs
+++ Terminator/Managers/LoginManager.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Scripting;
+using ZG.UI;
 
 public sealed class LoginManager : MonoBehaviour
 {
@@ -9,11 +11,21 @@ public sealed class LoginManager : MonoBehaviour
     internal struct Level
     {
         public string name;
+        public string title;
         public Sprite sprite;
     }
 
     [SerializeField]
     internal UnityEvent _onStart;
+
+    [SerializeField]
+    internal StringEvent _onEnergyMax;
+    
+    [SerializeField]
+    internal StringEvent _onEnergy;
+
+    [SerializeField] 
+    internal Progressbar _energy;
 
     [SerializeField]
     internal LevelStyle _style;
@@ -25,6 +37,41 @@ public sealed class LoginManager : MonoBehaviour
 
     private int __selectedIndex;
 
+    public static uint? userID
+    {
+        get;
+
+        private set;
+    } = null;
+
+    public static LoginManager instance
+    {
+        get;
+
+        private set;
+    }
+
+    public int gold
+    {
+        get;
+
+        set;
+    }
+
+    public int energy
+    {
+        get;
+
+        set;
+    }
+
+    public int energyMax
+    {
+        get;
+
+        private set;
+    }
+    
     [Preserve]
     public void LoadScene()
     {
@@ -37,9 +84,32 @@ public sealed class LoginManager : MonoBehaviour
         //GameAssetManager.instance.LoadScene(_levels[__selectedIndex].name, null);
     }
 
-    void OnEnable()
+    IEnumerator Start()
     {
+        instance = this;
+
+        int level = -1;
+        var userData = IUserData.instance;
+        if (userData == null)
+            userID = 0;
+        else
+        {
+            yield return userData.QueryUser(GameUser.Shared.channelName, GameUser.Shared.channelUser, (user, energy) =>
+            {
+                userID = user.id;
+                gold = user.gold;
+                level = user.level;
+
+                this.energy = energy.value;
+
+                energyMax = energy.max;
+
+            });
+        }
+        
         int numLevels = _levels == null ? 0 : _levels.Length;
+        if (level > 0)
+            numLevels = Mathf.Min(numLevels, level);
 
         Transform parent = _style.transform.parent;
         LevelStyle style;
