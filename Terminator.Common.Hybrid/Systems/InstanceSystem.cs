de71a4e52a7fee0fc51fb7af569df30f2a9a6e7e
linfuqing <linfuqing@126.com>
2025-03-18 13:02:05 +0800

    version 0.20.189

--- Terminator.Common.Hybrid/Systems/InstanceSystem.cs
+++ Terminator.Common.Hybrid/Systems/InstanceSystem.cs
@@ -4,21 +4,115 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Entities.Serialization;
 using Unity.Mathematics;
-using Unity.Scenes;
 using Unity.Transforms;
 
-[CreateAfter(typeof(PrefabLoaderSystem)), 
+public struct InstanceSingleton : IComponentData
+{
+    private NativeList<int> __idsToDisable;
+    private NativeList<CopyMatrixToTransformInstanceID> __idsToDestroy;
+
+    private NativeParallelMultiHashMap<FixedString128Bytes, Entity> __entities;
+    
+    private NativeParallelMultiHashMap<int, EntityPrefabReference> __entityPrefabReferences;
+
+    private NativeParallelMultiHashMap<EntityPrefabReference, RigidTransform> __loaders;
+    
+    internal InstanceSingleton(
+        NativeList<int> idsToDisable, 
+        NativeList<CopyMatrixToTransformInstanceID> idsToDestroy, 
+        NativeParallelMultiHashMap<FixedString128Bytes, Entity> entities, 
+        NativeParallelMultiHashMap<int, EntityPrefabReference> entityPrefabReferences, 
+        NativeParallelMultiHashMap<EntityPrefabReference, RigidTransform> loaders)
+    {
+        __idsToDisable = idsToDisable;
+        __idsToDestroy = idsToDestroy;
+        __entities = entities;
+        __entityPrefabReferences = entityPrefabReferences;
+        __loaders = loaders;
+    }
+    
+    public void Update(
+        SystemBase system, 
+        in ComponentLookup<LocalToWorld> localToWorlds, 
+        ref ComponentLookup<CopyMatrixToTransformInstanceID> instanceIDs)
+    {
+        if (!__idsToDisable.IsEmpty)
+        {
+            foreach (var id in __idsToDisable)
+            {
+                __entityPrefabReferences.Remove(id);
+
+                InstanceManager.Destroy(id, false);
+            }
+
+            __idsToDisable.Clear();
+        }
+
+        if (!__idsToDestroy.IsEmpty)
+        {
+            UnityEngine.Transform transform;
+            EntityPrefabReference entityPrefabReference;
+            NativeParallelMultiHashMapIterator<int> iterator;
+            foreach (var id in __idsToDestroy)
+            {
+                if (__entityPrefabReferences.TryGetFirstValue(id.value, out entityPrefabReference,
+                        out iterator))
+                {
+                    transform = UnityEngine.Resources.InstanceIDToObject(id.value) as UnityEngine.Transform;
+                    if (transform != null)
+                    {
+                        do
+                        {
+                            __loaders.Add(entityPrefabReference,
+                                math.RigidTransform(transform.rotation, transform.position));
+                        } while (__entityPrefabReferences.TryGetNextValue(out entityPrefabReference, ref iterator));
+                    }
+
+                    __entityPrefabReferences.Remove(id.value);
+                }
+
+                InstanceManager.Destroy(id.value, id.isSendMessageOnDestroy);
+            }
+
+            __idsToDestroy.Clear();
+        }
+
+        if (!__entities.IsEmpty)
+        {
+            using (var names = __entities.GetKeyArray(Allocator.Temp))
+            using (var entities = new NativeList<Entity>(Allocator.Temp))
+            {
+                int count = names.Unique();
+                FixedString128Bytes name;
+                for (int i = 0; i < count; ++i)
+                {
+                    name = names[i];
+
+                    entities.Clear();
+                    foreach (var entity in __entities.GetValuesForKey(name))
+                        entities.Add(entity);
+
+                    InstanceManager.Instantiate(
+                        name.ToString(),
+                        system,
+                        entities.AsArray(),
+                        localToWorlds,
+                        ref instanceIDs);
+                }
+            }
+
+            __entities.Clear();
+        }
+    }
+}
+
+[BurstCompile, 
+ CreateAfter(typeof(PrefabLoaderSystem)), 
  CreateAfter(typeof(CopyMatrixToTransformSystem)), 
  UpdateInGroup(typeof(InitializationSystemGroup))/*,
  UpdateAfter(typeof(MessageSystem))*/]
-public partial class InstanceSystem : SystemBase
+public partial struct InstanceSystemUnmanaged : ISystem
 {
-    /*private struct Scene
-    {
-        public int count;
-        public Entity entity;
-    }*/
-    
     private struct Save
     {
         [ReadOnly]
@@ -102,7 +196,53 @@ public partial class InstanceSystem : SystemBase
                 collect.Execute(i);
         }
     }
+
+    [BurstCompile]
+    private struct CollectIDs : IJobChunk
+    {
+        [ReadOnly]
+        public ComponentTypeHandle<CopyMatrixToTransformInstanceID> idType;
+
+        public NativeList<CopyMatrixToTransformInstanceID> ids;
+        
+        public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+        {
+            var ids = chunk.GetNativeArray(ref idType);
+
+            if (useEnabledMask)
+            {
+                var iterator = new ChunkEntityEnumerator(true, chunkEnabledMask, chunk.Count);
+                while (iterator.NextEntityIndex(out int i))
+                    this.ids.Add(ids[i]);
+            }
+            else
+                this.ids.AddRange(ids);
+        }
+    }
     
+    
+    [BurstCompile]
+    private struct Restore : IJobChunk
+    {
+        [ReadOnly]
+        public ComponentTypeHandle<CopyMatrixToTransformInstanceID> idType;
+
+        public BufferTypeHandle<InstancePrefab> prefabType;
+
+        public NativeList<int> ids;
+        
+        public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+        {
+            var ids = chunk.GetNativeArray(ref idType);
+            var iterator = new ChunkEntityEnumerator(true, chunkEnabledMask, chunk.Count);
+            while (iterator.NextEntityIndex(out int i))
+                this.ids.Add(ids[i].value);
+            
+            if(chunk.Has(ref prefabType))
+                chunk.SetComponentEnabledForAll(ref prefabType, true);
+        }
+    }
+
     private EntityTypeHandle __entityType;
 
     private BufferTypeHandle<InstancePrefab> __prefabType;
@@ -110,81 +250,81 @@ public partial class InstanceSystem : SystemBase
     private ComponentTypeHandle<CopyMatrixToTransformInstanceID> __idType;
         
     private ComponentTypeHandle<Instance> __instanceType;
-    private ComponentLookup<LocalToWorld> __localToWorlds;
     private ComponentLookup<LocalTransform> __localTransforms;
-    private ComponentLookup<CopyMatrixToTransformInstanceID> __instanceIDs;
 
     private EntityQuery __groupToEnable;
     private EntityQuery __groupToDisable;
     private EntityQuery __groupToDestroy;
     private EntityQuery __groupToCreate;
+    private NativeList<int> __idsToDisable;
+    private NativeList<CopyMatrixToTransformInstanceID> __idsToDestroy;
     private NativeParallelMultiHashMap<FixedString128Bytes, Entity> __entities;
     private NativeParallelMultiHashMap<int, EntityPrefabReference> __entityPrefabReferences;
     private NativeParallelMultiHashMap<EntityPrefabReference, RigidTransform> __loaders;
-    //private NativeHashMap<EntityPrefabReference, Scene> __scenes;
-    //private NativeHashMap<Entity, EntityPrefabReference> __instances;
     private PrefabLoader __prefabLoader;
 
-    protected override void OnCreate()
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
     {
-        base.OnCreate();
-
-        __entityType = GetEntityTypeHandle();
-        __prefabType = GetBufferTypeHandle<InstancePrefab>(true);
-        __idType = GetComponentTypeHandle<CopyMatrixToTransformInstanceID>(true);
-        __instanceType = GetComponentTypeHandle<Instance>(true);
-        __localToWorlds = GetComponentLookup<LocalToWorld>(true);
-        __localTransforms = GetComponentLookup<LocalTransform>();
-        __instanceIDs = GetComponentLookup<CopyMatrixToTransformInstanceID>();
+        __entityType = state.GetEntityTypeHandle();
+        __prefabType = state.GetBufferTypeHandle<InstancePrefab>();
+        __idType = state.GetComponentTypeHandle<CopyMatrixToTransformInstanceID>(true);
+        __instanceType = state.GetComponentTypeHandle<Instance>(true);
+        __localTransforms = state.GetComponentLookup<LocalTransform>();
 
         using (var builder = new EntityQueryBuilder(Allocator.Temp))
             __groupToEnable = builder
                 .WithAll<CopyMatrixToTransformInstanceID, InstancePrefab>()
-                .Build(this);
+                .Build(ref state);
 
         using (var builder = new EntityQueryBuilder(Allocator.Temp))
             __groupToDisable = builder
                 .WithAll<Disabled, CopyMatrixToTransformInstanceID>()
                 .WithPresent<Instance>()
                 .WithOptions(EntityQueryOptions.IncludeDisabledEntities)
-                .Build(this);
+                .Build(ref state);
 
         using (var builder = new EntityQueryBuilder(Allocator.Temp))
             __groupToDestroy = builder
                 .WithAll<CopyMatrixToTransformInstanceID>()
                 .WithAbsent<Instance>()
                 //.WithNone<Message>()
-                .Build(this);
+                .Build(ref state);
 
         using (var builder = new EntityQueryBuilder(Allocator.Temp))
             __groupToCreate = builder
                 .WithAll<LocalTransform>()
                 .WithAllRW<Instance>()
                 //.WithNone<CopyMatrixToTransformInstanceID>()
-                .Build(this);
+                .Build(ref state);
 
+        __idsToDisable = new NativeList<int>(Allocator.Persistent);
+        __idsToDestroy = new NativeList<CopyMatrixToTransformInstanceID>(Allocator.Persistent);
         __entities = new NativeParallelMultiHashMap<FixedString128Bytes, Entity>(1, Allocator.Persistent);
         __entityPrefabReferences = new NativeParallelMultiHashMap<int, EntityPrefabReference>(1, Allocator.Persistent);
         __loaders = new NativeParallelMultiHashMap<EntityPrefabReference, RigidTransform>(1, Allocator.Persistent);
 
-        __prefabLoader = new PrefabLoader(this);
+        __prefabLoader = new PrefabLoader(ref state);
+
+        state.EntityManager.CreateSingleton(__CreateSingleton());
     }
 
-    protected override void OnDestroy()
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
     {
+        __idsToDisable.Dispose();
+        __idsToDestroy.Dispose();
         __loaders.Dispose();
         __entityPrefabReferences.Dispose();
         __entities.Dispose();
-        
-        base.OnDestroy();
     }
 
-    protected override void OnUpdate()
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
     {
-        CompleteDependency();
+        state.CompleteDependency();
         
-        var world = World.Unmanaged;
-        var entityManager = world.EntityManager;
+        var entityManager = state.EntityManager;
         
         if (!__loaders.IsEmpty)
         {
@@ -223,7 +363,7 @@ public partial class InstanceSystem : SystemBase
 
                 if (entities.IsCreated)
                 {
-                    __localTransforms.Update(this);
+                    __localTransforms.Update(ref state);
 
                     LocalTransform localTransform;
                     localTransform.Scale = 1.0f;
@@ -245,11 +385,12 @@ public partial class InstanceSystem : SystemBase
             }
         }
         
+        __idType.Update(ref state);
+        
+        __prefabType.Update(ref state);
+
         if (!__groupToEnable.IsEmpty)
         {
-            __prefabType.Update(this);
-            __idType.Update(this);
-
             SaveEx save;
             save.idType = __idType;
             save.prefabType = __prefabType;
@@ -261,7 +402,13 @@ public partial class InstanceSystem : SystemBase
         
         if (!__groupToDisable.IsEmpty)
         {
-            using (var ids =
+            Restore restore;
+            restore.idType = __idType;
+            restore.prefabType = __prefabType;
+            restore.ids = __idsToDisable;
+            restore.RunByRef(__groupToDisable);
+
+            /*using (var ids =
                    __groupToDisable.ToComponentDataArray<CopyMatrixToTransformInstanceID>(Allocator.Temp))
             {
                 foreach (var id in ids)
@@ -270,16 +417,21 @@ public partial class InstanceSystem : SystemBase
                     
                     InstanceManager.Destroy(id.value, false);
                 }
-            }
+            }*/
 
-            EntityManager.SetComponentEnabled<Instance>(__groupToDisable, true);
+            entityManager.SetComponentEnabled<Instance>(__groupToDisable, true);
             
-            EntityManager.RemoveComponent<CopyMatrixToTransformInstanceID>(__groupToDisable);
+            entityManager.RemoveComponent<CopyMatrixToTransformInstanceID>(__groupToDisable);
         }
-
+        
         if (!__groupToDestroy.IsEmpty)
         {
-            using (var ids =
+            CollectIDs collectIDs;
+            collectIDs.idType = __idType;
+            collectIDs.ids = __idsToDestroy;
+            collectIDs.RunByRef(__groupToDestroy);
+            
+            /*using (var ids =
                    __groupToDestroy.ToComponentDataArray<CopyMatrixToTransformInstanceID>(Allocator.Temp))
             {
                 UnityEngine.Transform transform;
@@ -304,17 +456,17 @@ public partial class InstanceSystem : SystemBase
 
                     InstanceManager.Destroy(id.value, id.isSendMessageOnDestroy);
                 }
-            }
+            }*/
 
-            EntityManager.RemoveComponent<CopyMatrixToTransformInstanceID>(__groupToDestroy);
+            entityManager.RemoveComponent<CopyMatrixToTransformInstanceID>(__groupToDestroy);
         }
-
+        
         if (!__groupToCreate.IsEmpty)
         {
-            __entityType.Update(this);
-            __instanceType.Update(this);
+            __entityType.Update(ref state);
+            __instanceType.Update(ref state);
 
-            __entities.Clear();
+            //__entities.Clear();
 
             CollectEx collect;
             collect.entityType = __entityType;
@@ -322,7 +474,7 @@ public partial class InstanceSystem : SystemBase
             collect.entities = __entities;
             collect.RunByRef(__groupToCreate);
 
-            using (var names = __entities.GetKeyArray(Allocator.Temp))
+            /*using (var names = __entities.GetKeyArray(Allocator.Temp))
             using (var entities = new NativeList<Entity>(Allocator.Temp))
             {
                 int count = names.Unique();
@@ -337,47 +489,43 @@ public partial class InstanceSystem : SystemBase
 
                     InstanceManager.Instantiate(
                         name.ToString(),
-                        this,
+                        system,
                         entities.AsArray(),
                         __localToWorlds,
                         ref __instanceIDs);
                 }
-            }
+            }*/
 
-            EntityManager.SetComponentEnabled<Instance>(__groupToCreate, false);
-            //EntityManager.RemoveComponent<Instance>(__group);
-            //EntityManager.DestroyEntity(__group);
+            entityManager.SetComponentEnabled<Instance>(__groupToCreate, false);
         }
+    }
+
+    private InstanceSingleton __CreateSingleton()
+    {
+        return new InstanceSingleton(__idsToDisable, __idsToDestroy, __entities,
+            __entityPrefabReferences, __loaders);
     }
 }
 
-/*[UpdateInGroup(typeof(InitializationSystemGroup)), UpdateBefore(typeof(InstanceCreateSystem))]
-public partial class InstanceDestroySystem : SystemBase
+[UpdateInGroup(typeof(InitializationSystemGroup)), 
+ UpdateAfter(typeof(InstanceSystemUnmanaged))]
+public partial class InstanceSystem : SystemBase
 {
-    private EntityQuery __group;
-
+    private ComponentLookup<LocalToWorld> __localToWorlds;
+    private ComponentLookup<CopyMatrixToTransformInstanceID> __instanceIDs;
+    
     protected override void OnCreate()
     {
         base.OnCreate();
-
-        using (var builder = new EntityQueryBuilder(Allocator.Temp))
-            __group = builder
-                .WithAllRW<CopyMatrixToTransformInstanceID>()
-                .WithNone<LocalTransform>()
-                .Build(this);
         
-        RequireForUpdate(__group);
+        __localToWorlds = GetComponentLookup<LocalToWorld>(true);
+        __instanceIDs = GetComponentLookup<CopyMatrixToTransformInstanceID>();
+        
+        RequireForUpdate<InstanceSingleton>();
     }
-
+    
     protected override void OnUpdate()
     {
-        using (var instanceIDs = __group.ToComponentDataArray<CopyMatrixToTransformInstanceID>(Allocator.Temp))
-        {
-            var mananger = InstanceManager.instance;
-            foreach (var instanceID in instanceIDs)
-                mananger.Destroy(instanceID.value);
-        }
-
-        EntityManager.RemoveComponent<CopyMatrixToTransformInstanceID>(__group);
+        SystemAPI.GetSingleton<InstanceSingleton>().Update(this, __localToWorlds, ref __instanceIDs);
     }
-}*/
+}
