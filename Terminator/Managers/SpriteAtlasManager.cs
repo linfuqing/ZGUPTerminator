bef708ea194e32416a76841148e02b50a209d236
linfuqing <linfuqing@126.com>
2025-09-23 11:22:48 +0800

    Update SpriteAtlasManager.cs

--- Terminator/Managers/SpriteAtlasManager.cs
+++ Terminator/Managers/SpriteAtlasManager.cs
@@ -28,20 +28,23 @@ public class SpriteAtlasManager : MonoBehaviour
         int numAssets = _assets == null ? 0 : _assets.Length;
         __loaders = new AssetBundleLoader<SpriteAtlas>[numAssets];
         
-        var assetManager = GameAssetManager.instance.dataManager;
-        AssetBundleLoader<SpriteAtlas> loader;
-        Asset asset;
-        for(int i = 0; i < numAssets; ++i)
+        var assetManager = GameAssetManager.instance?.dataManager;
+        if (assetManager != null)
         {
-            asset = _assets[i];
-            
-            loader = new AssetBundleLoader<SpriteAtlas>(asset.filename, asset.name, assetManager);
+            AssetBundleLoader<SpriteAtlas> loader;
+            Asset asset;
+            for (int i = 0; i < numAssets; ++i)
+            {
+                asset = _assets[i];
 
-            __loaders[i] = loader;
-            
-            yield return loader;
+                loader = new AssetBundleLoader<SpriteAtlas>(asset.filename, asset.name, assetManager);
+
+                __loaders[i] = loader;
+
+                yield return loader;
+            }
         }
-        
+
         if(_onAllAssetsLoaded != null)
             _onAllAssetsLoaded.Invoke();
     }
