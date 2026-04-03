c6987eda0686afbb61a27ce76a81495c5a55881a
linfuqing <linfuqing@126.com>
2026-04-03 20:11:30 +0800

    version 1.25.595

--- Terminator/Managers/LoginManager.cs
+++ Terminator/Managers/LoginManager.cs
@@ -2004,14 +2004,6 @@ public sealed class LoginManager : MonoBehaviour
                         clientData.EndSend(writer);
                     }*/
                 }
-                else
-                {
-                    print("[Start]Not a host.");
-
-                    __status = Status.None;
-                    
-                    yield break;
-                }
             }
             else
             {
