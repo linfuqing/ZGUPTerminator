73493e3469e5ea1e1d543f1305bdd56b2109d8be
linfuqing <linfuqing@126.com>
2026-04-10 12:01:04 +0800

    Update AvatarDatabase.cs

--- Terminator.Core.Hybrid/Databases/AvatarDatabase.cs
+++ Terminator.Core.Hybrid/Databases/AvatarDatabase.cs
@@ -1,7 +1,9 @@
 using System;
 using Unity.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using ZG;
 
 [CreateAssetMenu(fileName = "New AvatarDatabase", menuName = "Game/AvatarDatabase")]
 public class AvatarDatabase : ScriptableObject
@@ -11,11 +13,40 @@ public class AvatarDatabase : ScriptableObject
     {
         public string name;
         public Sprite sprite;
-    }
+        
+#if UNITY_EDITOR
+        [CSVField]
+        public string 头像名称
+        {
+            set
+            {
+                name = value;
+            }
+        }
 
+        [CSVField]
+        public string 头像图标
+        {
+            set
+            {
+                sprite = AssetDatabase.LoadAssetAtPath<Sprite>(value);
+            }
+        }
+#endif
+    }
+    
+    [SerializeField]
+    internal Sprite _defaultSprite;
+    
     [SerializeField]
     internal Item[] _items;
 
+#if UNITY_EDITOR
+    [CSV("_items", guidIndex = -1, nameIndex = 0)] 
+    [SerializeField]
+    internal string _itemsPath;
+#endif
+    
     private Dictionary<FixedString32Bytes, Sprite> __items;
 
     public Sprite Get(in FixedString32Bytes name)
@@ -28,6 +59,6 @@ public class AvatarDatabase : ScriptableObject
                 __items.Add(item.name, item.sprite);
         }
         
-        return __items.TryGetValue(name, out var sprite) ? sprite : null;
+        return __items.TryGetValue(name, out var sprite) ? sprite : _defaultSprite;
     }
 }
