4427fb0c0b8a04d831feb460108372d16ec23bcf
linfuqing <linfuqing@126.com>
2026-04-10 11:46:55 +0800

    version 1.25.610

--- Terminator.Core.Hybrid/Managers/AvatarManager.cs
+++ Terminator.Core.Hybrid/Managers/AvatarManager.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
 
 public class AvatarManager : MonoBehaviour
 {
-    public static AvatarManager instance;
+    [SerializeField]
+    internal StringEvent _onLocalName;
+    [SerializeField]
+    internal SpriteEvent _onLocalAvatar;
+    [SerializeField]
+    internal SpriteEvent _onRemoteAvatar;
+    [SerializeField]
+    internal StringEvent _onRemoteName;
 
+    [SerializeField]
     internal AvatarDatabase _database;
 
+    public static AvatarManager instance
+    {
+        get;
+
+        private set;
+    }
+
     public static Sprite Get(string name) => instance._database.Get(name);
+
+    public void Apply()
+    {
+        {
+            ref var header = ref LevelPlayerShared<LocalPlayer>.header;
+            _onLocalName?.Invoke(header.name.ToString());
+            _onLocalAvatar?.Invoke(Get(header.avatar.ToString()));
+        }
+        
+        if (RemotePlayer.status >= RemotePlayer.Status.Joined)
+        {
+            ref var header = ref LevelPlayerShared<RemotePlayer>.header;
+            _onRemoteName?.Invoke(header.name.ToString());
+            _onRemoteAvatar?.Invoke(Get(header.avatar.ToString()));
+        }
+    }
 
     void Awake()
     {
         instance = this;
+    }
+
+    void Start()
+    {
+        Apply();
     }
 }
