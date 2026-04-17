73929cd63e1a8808118d39cbf7e2fe27d95602cd
linfuqing <linfuqing@126.com>
2026-04-17 16:41:33 +0800

    -

---
+++ Terminator/Controllers/CameraRotationController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotationController : MonoBehaviour
+{
+    [SerializeField]
+    internal Camera _camera;
+    
+    void Start()
+    {
+        if(_camera == null)
+            _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        transform.rotation = _camera.transform.rotation;
+    }
+}
